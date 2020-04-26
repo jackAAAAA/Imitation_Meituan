753dfad3484095498872a1c1403244e0363aa3f8
jackAAAAA <1143865523@qq.com>
2020-04-26 14:23:16 +0800

    First commit.

---
+++ src/RootScene.js
@@ -0,0 +1,27 @@
+/**
+*Copyright (c) 2017-present, Daniel
+*All rights reserved.
+*
+*@flow
+*/
+
+import React, { PureComponent } from 'react'
+import { StyleSheet, View, Image, Text } from 'react-native'
+
+class RootScene extends PureComponent<{}> {
+
+    render() {
+        return (
+            <View>
+                <Text>RootScene</Text>
+            </View>
+        )
+    }
+
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default RootScene
